dc3ec472703c896b6adb94b4d0f46cdf80c13542
ArdIv-by <ard.iv@yandex.ru>
2023-02-03 19:53:38 +0300

    fix html and scss

--- app/js/main.js
+++ app/js/main.js
@@ -1,10 +1,11 @@
 $(function () {
 
-  $('.slider').slick({
+  $('.slider-review').slick({
     dots: true,
-    prevArrow: '<button type="button" class="slider__arrow slider__arrow--prev"><svg><use xlink: href = "images/sprite.svg#icon-left"></use></svg><span class="lnr lnr-chevron-left"></span > </button>',
-    nextArrow: '<button type="button"  class="slider__arrow slider__arrow--next"><svg><use xlink: href = "images/sprite.svg#icon-right"></use></svg><span class="lnr lnr-chevron-right"></span></button>',
-    appendArrows: '.slider'
+    infinite: false,
+    prevArrow: '<button type="button" class="slider-review__arrow slider-review__arrow--prev"><svg><use xlink: href = "images/sprite.svg#icon-left"></use></svg><span class="lnr lnr-chevron-left"></span > </button>',
+    nextArrow: '<button type="button"  class="slider-review__arrow slider-review__arrow--next"><svg><use xlink: href = "images/sprite.svg#icon-right"></use></svg><span class="lnr lnr-chevron-right"></span></button>',
+    appendArrows: '.slider-review'
   })
 
 

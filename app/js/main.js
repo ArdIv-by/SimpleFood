0fe1a9140dbca73bee2627b6105a3e0fcac2cd88
ArdIv-by <ard.iv@yandex.ru>
2023-02-11 18:48:23 +0300

    added catalog page

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,22 @@
 $(function () {
 
+
+  $('.catalog__sort, .catalog__show').styler();
+
+
+  $('.filter-price__input').ionRangeSlider({
+    type: "double",
+    onStart: function (data) {
+      $('.filter-price__from').text(data.from)
+      $('.filter-price__to').text(data.to)
+    },
+    onChange: function (data) {
+      $('.filter-price__from').text(data.from)
+      $('.filter-price__to').text(data.to)
+    },
+  });
+
+
   $('.slider-review').slick({
     dots: true,
     infinite: false,
@@ -9,27 +26,7 @@ $(function () {
   });
 
 
-  document.addEventListener('DOMContentLoaded', () => {
-
-    //Mobile Menu
-    const burger = document.querySelector('.burger'); //наша кнопка
-    const mobileMenu = document.querySelector('.menu'); //мобильное меню
-    const bodyLock = document.querySelector('body'); //ищем как селектор ТЕГА
-
-    burger.addEventListener('click', () => {
-      mobileMenu.classList.toggle('menu--active'); //когда меню открыто
-      if (mobileMenu.classList.contains('menu--active')) { //Проверяем, есть ли у меню активный класс
-        burger.classList.add('burger--active'); //Когда открыто, иконка становится крестиком
-        bodyLock.classList.add('lock'); //Блокируем скролл при открытом меню
-      } else { //Когда нету активного класса у меню
-        burger.classList.remove('burger--active'); //Возвращает в исходное состояние
-        bodyLock.classList.remove('lock'); //Разрешаем скроллить
-      }
-    });
-  });
-
-
 
 });
 
-var mixer = mixitup('.categories__content');
+var mixer = mixitup('.categories__content, .catalog__content');

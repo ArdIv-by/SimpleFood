96c26792ddf33c45c26917669d8455d0585eb921
ArdIv-by <ard.iv@yandex.ru>
2023-02-19 21:00:38 +0300

    add product section

--- app/js/main.js
+++ app/js/main.js
@@ -1,7 +1,7 @@
 $(function () {
 
 
-  $('.catalog__sort, .catalog__show').styler();
+  $('.catalog__sort, .catalog__show, .product-item__num').styler();
 
 
   $('.filter-price__input').ionRangeSlider({
@@ -21,12 +21,26 @@ $(function () {
     dots: true,
     infinite: false,
     prevArrow: '<button type="button" class="slider-review__arrow slider-review__arrow--prev"><svg><use xlink: href = "images/sprite.svg#icon-left"></use></svg><span class="lnr lnr-chevron-left"></span > </button>',
-    nextArrow: '<button type="button"  class="slider-review__arrow slider-review__arrow--next"><svg><use xlink: href = "images/sprite.svg#icon-right"></use></svg><span class="lnr lnr-chevron-right"></span></button>',
+    nextArrow: '<button type="button" class="slider-review__arrow slider-review__arrow--next"><svg><use xlink: href = "images/sprite.svg#icon-right"></use></svg><span class="lnr lnr-chevron-right"></span></button>',
     appendArrows: '.slider-review'
   });
 
+  $(".product-item__star").rateYo({
+    starWidth: "16px",
+    normalFill: "rgba(193, 193, 193, 0.3)",
+    ratedFill: "#FFB800",
+    spacing: "6px",
+    readOnly: true
+  });
 
+});
 
+var swiper = new Swiper(".swiper", {
+  slidesPerView: 'auto',
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
 });
 
 var mixer = mixitup('.categories__content, .catalog__content');

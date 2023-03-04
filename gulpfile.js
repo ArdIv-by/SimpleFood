4f8585f901bfe8d83325971001abc94b32faf96c
ArdIv-by <ard.iv@yandex.ru>
2023-03-04 17:55:57 +0300

    add module

--- gulpfile.js
+++ gulpfile.js
@@ -10,6 +10,8 @@ const concat = require('gulp-concat');
 const autoprefixer = require('gulp-autoprefixer');
 const uglify = require('gulp-uglify');
 const imagemin = require('gulp-imagemin');
+const rename = require('gulp-rename');
+const nunjucksRender = require('gulp-nunjucks-render');
 const svgSprite = require('gulp-svg-sprite');
 const cheerio = require('gulp-cheerio');
 const replace = require('gulp-replace');
@@ -25,12 +27,21 @@ function browsersync() {
   })
 }
 
+function nunjucks() {
+  return src('app/module/*.njk')
+    .pipe(nunjucksRender())
+    .pipe(dest('app'))
+    .pipe(browserSync.stream())
+};
+
 function styles() {
-  return src('app/scss/style.scss')
+  return src('app/scss/*.scss')
     .pipe(scss({
       outputStyle: 'compressed'
     }))
-    .pipe(concat('style.min.css'))
+    .pipe(rename({
+      suffix : '.min'
+    }))
     .pipe(autoprefixer({
       overrideBrowserslist: ['last 10 versions'],
       grid: true
@@ -123,6 +134,7 @@ function cleanDist() {
 
 function watching() {
   watch(['app/scss/**/*.scss'], styles);
+  watch(['app/*.njk'], nunjucks);
   watch(['app/js/**/*.js', '!app/js/main.min.js'], scripts);
   watch(['app/images/icons/*.svg'], svgSprites);
   watch(['app/*.html']).on('change', browserSync.reload)
@@ -136,9 +148,10 @@ exports.scripts = scripts;
 exports.browsersync = browsersync;
 exports.watching = watching;
 exports.images = images;
+exports.nunjucks = nunjucks;
 exports.svgSprites = svgSprites;
 exports.cleanDist = cleanDist;
 exports.build = series(cleanDist, images, build);
 
 
-exports.default = parallel(svgSprites, styles, scripts, browsersync, watching)
+exports.default = parallel(nunjucks, svgSprites, styles, scripts, browsersync, watching)
